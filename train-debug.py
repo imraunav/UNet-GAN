805641979d76e2cb3a48c22d12e449923aa3a526
imraunav <raun.ghosh@gmail.com>
2023-11-10 00:06:54 +0530

    In-channel rectified

--- train-debug.py
+++ train-debug.py
@@ -28,7 +28,7 @@ class CNNClassifier(nn.Module):
     def __init__(self, num_classes):
         super(CNNClassifier, self).__init__()
         # Convolutional layers
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
